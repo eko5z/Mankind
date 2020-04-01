43b4ff0f1ba0db570ca7eae901f96f22d0aab3b8
Adrien Jaguenet <adrien@localhost.localdomain>
2020-04-01 22:36:18 +0200

    Generate fewer blocks

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -8,11 +8,11 @@ void Chunk::Generate(int seed)
 	for (int i(0); i < CHUNK_SIZE; ++i) {
 		for (int j(0); j < CHUNK_SIZE; ++j) {
 			for (int k(0); k < CHUNK_SIZE; ++k) {
-				int n = rand() % 2;
+				int n = rand() % 100;
 				if (n) {
-					SetCube(i, j, k, Cube{1});
-				} else {
 					SetCube(i, j, k, Cube{0});
+				} else {
+					SetCube(i, j, k, Cube{1});
 				}
 			}
 		}
