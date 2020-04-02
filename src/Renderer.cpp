792ad49240fdd5c6129779a39b401fef7b23a6c2
Adrien Jaguenet <adrien@localhost.localdomain>
2020-04-03 01:51:06 +0200

    Superman eyes destroy cubes

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -150,6 +150,10 @@ void Renderer::Render(World& world, Camera& camera)
 
 		kc.second.Render();
 	}
+	CalculatePointing(world, position, lookat, 5.0);
+	if (is_pointing) {
+		world.SetCube(pointed_cube.x, pointed_cube.y, pointed_cube.z, Cube{0});
+	}
 
 	SDL_GL_SwapWindow(window);
 }
@@ -158,6 +162,97 @@ void Renderer::ComputeFrustrum(glm::vec3 position, glm::vec3 lookAt)
 {
 	static glm::vec3 up(0., 1., 0.), right(1., 0., 0.);
 	lookAt = glm::normalize(lookAt);
+}
+
+void Renderer::CalculatePointing(World& world, glm::vec3 position, glm::vec3 lookAt, float maxDistance)
+{
+	float _bin_size = 1;
+	is_pointing = false;
+	glm::vec3 ray_start(position);
+	// This id of the first/current voxel hit by the ray.
+	// Using floor (round down) is actually very important,
+	// the implicit int-casting will round up for negative numbers.
+	glm::vec3 current_voxel(std::floor(ray_start[0]/_bin_size),
+	                        std::floor(ray_start[1]/_bin_size),
+	                        std::floor(ray_start[2]/_bin_size));
+
+	glm::vec3 ray_end(position + glm::normalize(lookAt) * maxDistance);
+	// The id of the last voxel hit by  = current_voxelthe ray.
+	// TODO: what happens if the end point is on a border?
+	glm::vec3 last_voxel(std::floor(ray_end[0]/_bin_size),
+	                     std::floor(ray_end[1]/_bin_size),
+	                     std::floor(ray_end[2]/_bin_size));
+
+	// Compute normalized ray direction.
+	glm::vec3 ray = ray_end-ray_start;
+	//ray.normalize();
+
+	// In which direction the voxel ids are incremented.
+	float stepX = (ray[0] >= 0) ? 1:-1; // correct
+	float stepY = (ray[1] >= 0) ? 1:-1; // correct
+	float stepZ = (ray[2] >= 0) ? 1:-1; // correct
+
+	// Distance along the ray to the next voxel border from the current position (tMaxX, tMaxY, tMaxZ).
+	float next_voxel_boundary_x = (current_voxel[0]+stepX)*_bin_size; // correct
+	float next_voxel_boundary_y = (current_voxel[1]+stepY)*_bin_size; // correct
+	float next_voxel_boundary_z = (current_voxel[2]+stepZ)*_bin_size; // correct
+
+	// tMaxX, tMaxY, tMaxZ -- distance until next intersection with voxel-border
+	// the value of t at which the ray crosses the first vertical voxel boundary
+	float tMaxX = (ray[0]!=0) ? (next_voxel_boundary_x - ray_start[0])/ray[0] : DBL_MAX; //
+	float tMaxY = (ray[1]!=0) ? (next_voxel_boundary_y - ray_start[1])/ray[1] : DBL_MAX; //
+	float tMaxZ = (ray[2]!=0) ? (next_voxel_boundary_z - ray_start[2])/ray[2] : DBL_MAX; //
+
+	// tDeltaX, tDeltaY, tDeltaZ --
+	// how far along the ray we must move for the horizontal component to equal the width of a voxel
+	// the direction in which we traverse the grid
+	// can only be FLT_MAX if we never go in that direction
+	float tDeltaX = (ray[0]!=0) ? _bin_size/ray[0]*stepX : DBL_MAX;
+	float tDeltaY = (ray[1]!=0) ? _bin_size/ray[1]*stepY : DBL_MAX;
+	float tDeltaZ = (ray[2]!=0) ? _bin_size/ray[2]*stepZ : DBL_MAX;
+
+	glm::vec3 diff(0,0,0);
+	bool neg_ray=false;
+	if (current_voxel[0]!=last_voxel[0] && ray[0]<0) {
+		diff[0]--;
+		neg_ray=true;
+	}
+	if (current_voxel[1]!=last_voxel[1] && ray[1]<0) {
+		diff[1]--;
+		neg_ray=true;
+	}
+	if (current_voxel[2]!=last_voxel[2] && ray[2]<0) {
+		diff[2]--;
+		neg_ray=true;
+	}
+	if (neg_ray) {
+		current_voxel+=diff;
+	}
+
+	while(last_voxel != current_voxel) {
+		if (tMaxX < tMaxY) {
+			if (tMaxX < tMaxZ) {
+				current_voxel[0] += stepX;
+				tMaxX += tDeltaX;
+			} else {
+				current_voxel[2] += stepZ;
+				tMaxZ += tDeltaZ;
+			}
+		} else {
+			if (tMaxY < tMaxZ) {
+				current_voxel[1] += stepY;
+				tMaxY += tDeltaY;
+			} else {
+				current_voxel[2] += stepZ;
+				tMaxZ += tDeltaZ;
+			}
+		}
+		if (world.GetCube(current_voxel.x, current_voxel.y, current_voxel.z).typeID != 0) {
+			is_pointing = true;
+			pointed_cube = current_voxel;
+			return;
+		}
+	}
 }
 
 void Renderer::AddChunk(World& world, int x, int y, int z, Chunk& c)
