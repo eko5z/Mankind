7042267a4dd28e44533b2df519f63d8a775cc648
Emil Kosz <emssej@gmail.com>
2020-04-05 18:11:21 +0200

    Midget ass broken code

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -16,7 +16,6 @@ Renderer::Renderer(Game& g) :
 	sun(glm::vec3{0.3, 1.0, 0.3},
 	    glm::vec3{0.5, 0.5, 0.5},
 	    glm::vec3{1.0, 1.0, 1.0}),
-	sky(nullptr),
 	n_frames(0),
 	ms_accu(0),
 	last_time(0),
@@ -98,10 +97,12 @@ void Renderer::OpenWindow()
 	this->sky_program = std::make_unique<Program>("res/shaders/sky.vert", "res/shaders/sky.frag");
 	this->text_program = std::make_unique<Program>("res/shaders/text.vert", "res/shaders/text.frag");
 	this->highlight_program = std::make_unique<Program>("res/shaders/default.vert", "res/shaders/highlight.frag");
-	this->sky = std::make_unique<Sky>();
 	uniform_mvp = default_program->GetUniform("MVP");
 
-	highlight_mesh = std::make_unique<HighlightMesh>();
+	this->highlight_mesh = std::make_unique<HighlightMesh>();
+	LOG ("Initialized highlight mesh.");
+	this->sky = std::make_unique<Sky>();
+	LOG ("Initialized sky.");
 
 	LOG("Window correctly opened");
 
@@ -147,7 +148,7 @@ void Renderer::Render()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_POLYGON_OFFSET_FILL);
 
-	DrawSky();
+	// DrawSky();
 	DrawTerrain();
 	DrawHighlight();
 	DrawGUI();
@@ -164,7 +165,6 @@ void Renderer::AddChunk(int x, int y, int z, Chunk& c)
 void Renderer::DrawSky()
 {
 	Camera& camera = game.GetCamera();
-	glDisable(GL_DEPTH_TEST);
 
 	this->sky_program->Use();
 	this->sky_program->SetVec3("sun_direction", this->sun.direction);
@@ -173,8 +173,6 @@ void Renderer::DrawSky()
 	this->sky_program->SetFloat("vertical_fov", v_fov_rad);
 	this->sky_program->SetFloat("horizontal_fov", deg2rad(h_fov));
 	this->sky->Render();
-
-	glEnable(GL_DEPTH_TEST);
 }
 
 void Renderer::DrawTerrain()
--- src/Sky.cpp
+++ src/Sky.cpp
@@ -1,67 +1,45 @@
 #include "Sky.hpp"
 
+//   2---4
+//   |\  |
+//   | \ |
+//   |  \|
+//   1___3
+
+/// Generate the fullscreen quad.
+
+
+#define v1 glm::vec3{-1.0, -1.0, 0.0}
+#define v2 glm::vec3{-1.0, 1.0, 0.0}
+#define v3 glm::vec3{1.0, -1.0, 0.0}
+#define v4 glm::vec3{1.0, 1.0, 0.0}
+
+#define u1 glm::vec2{0, 0}
+#define u2 glm::vec2{0, 1}
+#define u3 glm::vec2{1, 0}
+#define u4 glm::vec2{1, 1}
+
+std::vector<glm::vec3> vertices{v1, v2, v3, v3, v2, v4};
+
+// We don't really need them, but whatever.
+std::vector<glm::vec3> normals{6, glm::vec3{0.0, 0.0, -1.0}};
+
+std::vector<glm::vec2> uvs{u1, u2, u3, u2, u4, u3};
+
+std::vector<GLuint> indices{0, 1, 2, 3, 4, 5, 6};
+
 Sky::Sky():
-	mesh(nullptr)
+  Mesh::Mesh(
+	     vertices, normals, uvs, indices,
+	     nullptr, nullptr
+	     )
 {
 
-	//   2---4
-	//   |\  |
-	//   | \ |
-	//   |  \|
-	//   1___3
-
-	/// Generate the fullscreen quad.
-	std::vector<glm::vec3> vertices;
-
-	glm::vec3 v1(-1.0, -1.0, 0.0);
-	glm::vec3 v2(-1.0, 1.0, 0.0);
-	glm::vec3 v3(1.0, -1.0, 0.0);
-	glm::vec3 v4(1.0, 1.0, 0.0);
-
-	vertices.push_back(v1);
-	vertices.push_back(v2);
-	vertices.push_back(v3);
-	vertices.push_back(v3);
-	vertices.push_back(v2);
-	vertices.push_back(v4);
-
-	// We don't really need them, but whatever.
-	std::vector<glm::vec3> normals;
-
-	normals.push_back(glm::vec3{0.0, 0.0, -1.0});
-	normals.push_back(glm::vec3{0.0, 0.0, -1.0});
-	normals.push_back(glm::vec3{0.0, 0.0, -1.0});
-
-	normals.push_back(glm::vec3{0.0, 0.0, -1.0});
-	normals.push_back(glm::vec3{0.0, 0.0, -1.0});
-	normals.push_back(glm::vec3{0.0, 0.0, -1.0});
-
-	std::vector<glm::vec2> uvs;
-
-	uvs.push_back(glm::vec2{0, 0});
-	uvs.push_back(glm::vec2{0, 1});
-	uvs.push_back(glm::vec2{1, 0});
-
-	uvs.push_back(glm::vec2{0, 1});
-	uvs.push_back(glm::vec2{1, 1});
-	uvs.push_back(glm::vec2{1, 0});
-
-	std::vector<GLuint> indices;
-
-	indices.push_back(0);
-	indices.push_back(1);
-	indices.push_back(2);
-
-	indices.push_back(3);
-	indices.push_back(4);
-	indices.push_back(5);
-
-	this->mesh = std::make_unique<Mesh>(vertices, normals, uvs, indices, nullptr, nullptr);
 }
 
 void Sky::Render()
 {
-	glDisable(GL_DEPTH_TEST);
-	this->mesh->Render();
-	glEnable(GL_DEPTH_TEST);
+  glDisable(GL_DEPTH_TEST);
+  Mesh::Render();
+  glEnable(GL_DEPTH_TEST);
 }
