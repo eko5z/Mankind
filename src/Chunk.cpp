73ade48be27f0ce0de11b2c18ec6f1a7cb6cf457
Emil Kosz <emssej@gmail.com>
2020-04-05 14:57:18 +0200

    Fixed chunk

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -8,19 +8,3 @@ Chunk::Chunk() :
 	empty(true)
 {
 }
-
-void Chunk::Generate(int x, int y, int z)
-{
-	this->x = x;
-	this->y = y;
-	this->z = z;
-
-	for (int i(0); i < CHUNK_SIZE; ++i) {
-		for (int j(0); j < CHUNK_SIZE; ++j) {
-			for (int k(0); k < CHUNK_SIZE; ++k) {
-				SetCube(i, j, k, Cube{0});
-			}
-		}
-	}
-}
-
