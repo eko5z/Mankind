d3910f4d02f41f80bcf7d5ffcf4732314e8e3658
Emil Kosz <emssej@gmail.com>
2020-04-05 18:35:56 +0200

    Fixed the segfault

--- src/Sky.cpp
+++ src/Sky.cpp
@@ -19,14 +19,14 @@
 #define u3 glm::vec2{1, 0}
 #define u4 glm::vec2{1, 1}
 
-std::vector<glm::vec3> vertices{v1, v2, v3, v3, v2, v4};
+#define vertices std::vector<glm::vec3>{v1, v2, v3, v3, v2, v4}
 
 // We don't really need them, but whatever.
-std::vector<glm::vec3> normals{6, glm::vec3{0.0, 0.0, -1.0}};
+#define normals std::vector<glm::vec3>{6, glm::vec3{0.0, 0.0, -1.0}}
 
-std::vector<glm::vec2> uvs{u1, u2, u3, u2, u4, u3};
+#define uvs std::vector<glm::vec2>{u1, u2, u3, u2, u4, u3}
 
-std::vector<GLuint> indices{0, 1, 2, 3, 4, 5, 6};
+#define indices std::vector<GLuint>{0, 1, 2, 3, 4, 5}
 
 Sky::Sky():
 	Mesh::Mesh(
